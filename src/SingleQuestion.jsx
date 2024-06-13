fd9a012308bb43cfb7d1ad175c8024bf65ea55b1
dollarp-code <husseindollarp@yahoo.com>
2024-06-13 09:01:07 +0100

    alternative logic

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const SingleQuestion = ({ title, info }) => {
-  const [showInfo, setShowInfo] = useState(false);
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+  // const [showInfo, setShowInfo] = useState(false);
+  const isActive = id === activeId;
 
   return (
     <article className="question">
@@ -11,12 +12,12 @@ const SingleQuestion = ({ title, info }) => {
         <button
           className="question-btn"
           type="button"
-          onClick={() => setShowInfo(!showInfo)}
+          onClick={() => toggleQuestion(id)}
         >
-          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {showInfo && <p>{info}</p>}
+      {isActive && <p>{info}</p>}
     </article>
   );
 };
